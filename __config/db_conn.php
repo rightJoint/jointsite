2f7e7868fbc667fe1387bd6ba05a3c20c08c6a0b
Alexey <rightjoint@yandex.ru>
2024-05-14 19:49:27 +0300

    db-conn

---
+++ __config/db_conn.php
@@ -0,0 +1,1 @@
+{"CONN_LOC":"localhost","CONN_USER":"root","CONN_PW":"","CONN_DB":"test_db"}
--- __config/dir_const.php
+++ __config/dir_const.php
@@ -1,2 +1,3 @@
 <?php
 define("JOINT_SITE_APP_CONFIG", JOINT_SITE_CONF_DIR."/app_config.php");
+define("SQL_CONN_DEFAULT", JOINT_SITE_CONF_DIR."/db_conn.php");
