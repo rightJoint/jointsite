e82ef85312ca10aaa797e6a5aee1b015a5ce3b0e
Alexey <rightjoint@yandex.ru>
2024-05-15 21:15:30 +0300

    conf

--- __config/admin_conf.php
+++ __config/admin_conf.php
@@ -1,5 +1,5 @@
 <?php
-define("PATH_TO_USR_LIST", JOINT_SITE_CONF_DIR."/admin/adminUsers.txt");
+define("PATH_TO_USR_LIST", JOINT_SITE_CONF_DIR."/adminUsers.txt");
 define("PATH_TO_TABLES_LIST", $_SERVER["DOCUMENT_ROOT"].
     JOINT_SITE_EXEC_DIR."/admin_db/createQueries");    // where look up create query files
 define("PATH_TO_DB_UPLOAD", $_SERVER["DOCUMENT_ROOT"].
