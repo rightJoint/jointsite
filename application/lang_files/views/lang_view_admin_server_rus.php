dce68804def45bac16b073f317dc6422ad3db6e6
Alexey <rightjoint@yandex.ru>
2024-05-16 11:18:42 +0300

    lang-server

---
+++ application/lang_files/views/lang_view_admin_server_rus.php
@@ -0,0 +1,15 @@
+<?php
+class lang_view_admin_server_rus extends lang_view_admin_rus
+{
+    public $admin_server = array(
+        "state_txt" => "Состояние",
+        "conn_est_txt" => "Подключение к SQL базе данных",
+        "conn_est_stat" => "Установлено",
+        "conn_fail_txt" => "Проблема с подключением к SQL базе данных",
+        "conn_fail_stat" => "НЕУДАЧНО",
+        "debug_txt" => "отладочная информация",
+        "settings_txt" => "Настройки",
+        "settings_file" => "используемый файл настроек",
+        "save_btn" => "Сохранить",
+    );
+}
