d7b77c1b77c3a728e95f27d1ade058421c88580e
Alexey <rightjoint@yandex.ru>
2024-05-21 15:49:45 +0300

    prod-langs

--- application/lang_files/views/lang_view_jointsite_record_rus.php
+++ application/lang_files/views/lang_view_jointsite_record_rus.php
@@ -1,5 +1,5 @@
 <?php
-class lang_view_jointsite_main_rus extends lang_view_products_jointsite_rus
+class lang_view_jointsite_record_rus extends lang_view_products_jointsite_rus
 {
     function __construct()
     {
@@ -7,9 +7,8 @@ class lang_view_jointsite_main_rus extends lang_view_products_jointsite_rus
 
         $this->head["description"] = $this->head["h1"].". ".$this->head["title"];
 
-        $this->product_custom["p1"] = "Приложение написано на php и потддерживает MVC шаблон. ".
-            "В core-ветке main содержатся основные шаблоны для быстрого применения в дальнейшем на тематических ветках. ".
-            "Для педставления обрабатываемых ошибок приложения пердназначен Alerts_Controller. ";
+        $this->product_custom["p1"] = "Record содержит контроллер, модель и представления для работы с записями ".
+            "в таблицах базы данных. Все core-ветки, как и Record будут вливаться в ветку main.";
         $this->product_deploy["install"]["checkout-branch"] = "main";
         $this->product_deploy["install"]["example-text"] = "клонирование репозитория и переключение на ветку ".
             "(<strong>".$this->product_deploy["install"]["checkout-branch"]."</strong>)";
@@ -18,6 +17,7 @@ class lang_view_jointsite_main_rus extends lang_view_products_jointsite_rus
             "h3" => "Конфигурирование",
             "p1" => "Все основные настройки приложения по умолчанию находятся в каталоге  <span class='ex-conf'>__config</span>. ",
             "p2" => "В этой ветке никаких дополнительных настроек не требуется.",
+            "p3" => "В этой ветке никаких дополнительных настроек не требуется.",
         );
         $this->prod_test = array(
             "p1" => "Проверить работу приложения можно на <a href='".JOINT_SITE_EXEC_DIR."/test/main' ".
@@ -25,4 +25,6 @@ class lang_view_jointsite_main_rus extends lang_view_products_jointsite_rus
 
         );
     }
+
+
 }
