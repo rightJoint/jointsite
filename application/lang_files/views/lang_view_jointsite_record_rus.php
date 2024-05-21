e821eeb8eb3c077997900d9da574011db173f8cd
Alexey <rightjoint@yandex.ru>
2024-05-21 16:03:58 +0300

    prod-texts

--- application/lang_files/views/lang_view_jointsite_record_rus.php
+++ application/lang_files/views/lang_view_jointsite_record_rus.php
@@ -16,13 +16,18 @@ class lang_view_jointsite_record_rus extends lang_view_products_jointsite_rus
         $this->product_config = array(
             "h3" => "Конфигурирование",
             "p1" => "Все основные настройки приложения по умолчанию находятся в каталоге  <span class='ex-conf'>__config</span>. ",
-            "p2" => "В этой ветке никаких дополнительных настроек не требуется.",
-            "p3" => "В этой ветке никаких дополнительных настроек не требуется.",
+            "p2" => "Настройки подключения к базе данных по умолчанию находятся в  <span class='ex-conf'>db_conn.php</span>. ",
         );
         $this->prod_test = array(
             "p1" => "Проверить работу приложения можно на <a href='".JOINT_SITE_EXEC_DIR."/test/main' ".
                 "title='Как работает на тесте'>Проверить приложение</a>, но в этой ветке только шаблоны, которые не интересно проверять.",
 
+        );
+        $this->product_migration["p1"] = "Запросы на создание тестовых таблиц и вставки данных находятся в каталоге <span class='ex-conf'>/migrations</span>";
+        $this->prod_test = array(
+            "p1" => "Проведите миграцию <span class='ex-conf'>2024-05-21_rj-test-records</span>. ".
+                "<a href='".JOINT_SITE_EXEC_DIR."/test/record' title='Как работает Запись на тесте'>Проверить ветку Запись на тесте</a>. ".
+        "В примере используются кастомные модели из музкальной галлереи",
         );
     }
 
