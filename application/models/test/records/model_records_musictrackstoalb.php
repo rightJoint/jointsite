dfb3772cf2c953333ff583489b5fbbbe96181048
Alexey <rightjoint@yandex.ru>
2024-05-21 22:42:13 +0300

    test-tables

--- application/models/test/records/model_records_musictrackstoalb.php
+++ application/models/test/records/model_records_musictrackstoalb.php
@@ -2,7 +2,12 @@
 require_once $_SERVER["DOCUMENT_ROOT"].JOINT_SITE_EXEC_DIR."/application/recordsStructureFiles/test/rsf_musictrackstoalb.php";
 class model_records_musictrackstoalb extends RecordsModel
 {
-    public $tableName = "musicTracksToAlb_dt";
+    public $tableName = "rjt_musictrackstoalb";
+
+    public $albTable = "rjt_musicalb";
+    public $tracksTable = "rjt_musictracks";
+
+
     public $modelAliases = array(
         "en" => "music tracks to albums",
         "rus" => "мелодии в альбом"
@@ -15,13 +20,13 @@ class model_records_musictrackstoalb extends RecordsModel
 
     function copyCustomFields()
     {
-        $albName_qry = "select albumName from musicAlb_dt where album_id = '".$this->recordStructureFields->record["album_id"]["curVal"]."'";
+        $albName_qry = "select albumName from ".$this->albTable." where album_id = '".$this->recordStructureFields->record["album_id"]["curVal"]."'";
         $albName_res = $this->query($albName_qry);
         if($albName_res->rowCount() == 1){
             $this->recordStructureFields->record["albumName"]["curVal"] = $albName_res->fetch(self::FETCH_ASSOC)["albumName"];
         }
 
-        $trackName_qry = "select track_name from musicTracks_dt where track_id = '".$this->recordStructureFields->record["track_id"]["curVal"]."'";
+        $trackName_qry = "select track_name from ".$this->tracksTable." where track_id = '".$this->recordStructureFields->record["track_id"]["curVal"]."'";
         $trackName_res = $this->query($trackName_qry);
         if($trackName_res->rowCount() == 1){
             $this->recordStructureFields->record["track_name"]["curVal"] = $trackName_res->fetch(self::FETCH_ASSOC)["track_name"];
@@ -32,19 +37,19 @@ class model_records_musictrackstoalb extends RecordsModel
 
     public function listRecords($where = null, $order = null, $limit = null, $having = null)
     {
-        $findList_qry = "select musicTracksToAlb_dt.track_id, 
-musicTracksToAlb_dt.album_id, 
-musicTracksToAlb_dt.comment,
-musicTracksToAlb_dt.mActive,
-musicTracksToAlb_dt.sortDate, 
-musicTracks_dt.track_name, 
-musicTracks_dt.track_artist, 
-musicTracks_dt.loadDate, 
-musicAlb_dt.albumName, 
-musicAlb_dt.albumImg 
-from musicTracksToAlb_dt 
-left join musicTracks_dt on musicTracks_dt.track_id = musicTracksToAlb_dt.track_id 
-left join musicAlb_dt on musicTracksToAlb_dt.album_id = musicAlb_dt.album_id  ".
+        $findList_qry = "select ".$this->tableName.".track_id, 
+".$this->tableName.".album_id, 
+".$this->tableName.".comment,
+".$this->tableName.".mActive,
+".$this->tableName.".sortDate, 
+".$this->tracksTable.".track_name, 
+".$this->tracksTable.".track_artist, 
+".$this->tracksTable.".loadDate, 
+".$this->albTable.".albumName, 
+".$this->albTable.".albumImg 
+from ".$this->tableName." 
+left join ".$this->tracksTable." on ".$this->tracksTable.".track_id = ".$this->tableName.".track_id 
+left join ".$this->albTable." on ".$this->tableName.".album_id = ".$this->albTable.".album_id  ".
             $where.$order.$limit;
 
         return $this->fetchToArray($findList_qry);
@@ -53,8 +58,8 @@ left join musicAlb_dt on musicTracksToAlb_dt.album_id = musicAlb_dt.album_id  ".
     public function countRecords($where = null)
     {
         return $this->query("SELECT COUNT(*) as cnt from ".$this->tableName." ".
-            "left join musicTracks_dt on musicTracks_dt.track_id = musicTracksToAlb_dt.track_id 
-left join musicAlb_dt on musicTracksToAlb_dt.album_id = musicAlb_dt.album_id  ".
+            "left join ".$this->tracksTable." on ".$this->tracksTable.".track_id = ".$this->tableName.".track_id 
+left join ".$this->albTable." on ".$this->tableName.".album_id = ".$this->albTable.".album_id  ".
             $where)->fetch(PDO::FETCH_ASSOC)["cnt"];
     }
 
