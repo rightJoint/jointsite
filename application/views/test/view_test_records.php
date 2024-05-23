2bd6853be9182680c372107609b1be6fbf3dec7d
Alexey <rightjoint@yandex.ru>
2024-05-23 14:47:31 +0300

    text

--- application/views/test/view_test_records.php
+++ application/views/test/view_test_records.php
@@ -6,9 +6,9 @@ class view_test_records extends view_test
         echo "<div class='contentBlock-frame'><div class='contentBlock-center'>".
             "<div class='contentBlock-wrap'>".
             "<h3>Список тестов ветки Запись</h3>".
-            "<p>Чтобы открыть таблицу (список и записи), используйте адерес /table/table_name, где table_name - имя таблицы в БД</p>".
-            "<p>Например <a href='".JOINT_SITE_EXEC_DIR."/test/records/table/musictrackstoalb_dt'>".
-            JOINT_SITE_EXEC_DIR."/test/records/table/musictrackstoalb_dt"."</a> покажет таблицу musicalb как в примере 1, ".
+            "<p>Чтобы открыть таблицу (список и записи), используйте адерес <span class='ex-conf'>/table/table_name</span>, где table_name - имя таблицы в БД</p>".
+            "<p>Например <a href='".JOINT_SITE_EXEC_DIR."/test/records/table/rjt_musicalb'>".
+            JOINT_SITE_EXEC_DIR."/test/records/table/rjt_musicalb"."</a> покажет таблицу rjt_musicalb_dt как в примере 1, ".
             "но без кастомной модели.</p>".
             "<p>Для примера используются три кастомные модели</p>".
             "<ol>".
