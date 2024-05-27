bc2c68aa015f75aa0c80cde2fe83407f9233fd79
Alexey <rightjoint@yandex.ru>
2024-05-27 16:23:07 +0300

    rec-text

--- application/lang_files/views/templates/lang_view_RecordEdit_en.php
+++ application/lang_files/views/templates/lang_view_RecordEdit_en.php
@@ -2,20 +2,26 @@
 class lang_view_RecordEdit_en extends lang_view_en
 {
     public $view_submit_val = "Update";
+    public $view_submit_val_new = "New";
+    public $view_submit_val_del = "Delete";
 
     function update_head_array($options)
     {
         if($options["type"] == "edit"){
-            $txt_en = "Edit";
+            $txt_rus = "Edit";
+
             $this->view_submit_val = "Update";
 
         }elseif ($options["type"] == "new"){
-            $txt_en = "Create";
-            $this->view_submit_val = "Create";
+            $txt_rus = "Create";
+            $this->view_submit_val = $this->view_submit_val_new;
+        }elseif ($options["type"] == "delete"){
+            $txt_rus = "Delete";
+            $this->view_submit_val = $this->view_submit_val_del;
         }
 
-        $this->head["description"] = $txt_en." record in table";
-        $this->head["title"] = $txt_en." view in table ".$options["h2"];
-        $this->head["h1"] = $txt_en." view in table ".$options["h2"];
+        $this->head["description"] = $txt_rus." record in table";
+        $this->head["title"] = $txt_rus." record in table ".$options["h2"];
+        $this->head["h1"] = $txt_rus." record in table ".$options["h2"];
     }
 }
