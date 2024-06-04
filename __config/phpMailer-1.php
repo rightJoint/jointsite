646f701040526158a6536ec718a5973fcb867481
Alexey <rightjoint@yandex.ru>
2024-06-04 22:15:07 +0300

    php-mailer-add

---
+++ __config/phpMailer-1.php
@@ -0,0 +1,8 @@
+<?php
+define("PHP_MAILER_HOST_1", "smtp.yandex.ru");
+define("PHP_MAILER_SMTP_AUTH_1", true);
+define("PHP_MAILER_USER_NAME_1", "xxx@yandex.ru");// Ваш логин в Яндексе. Именно логин, без @yandex.ru
+define("PHP_MAILER_PASSWORD_1", "xxx");
+define("PHP_MAILER_PORT_1", 465);
+define("PHP_MAILER_FROM_1", PHP_MAILER_USER_NAME_1);// Ваш Email
+define("PHP_MAILER_IS_HTML_1", true);
