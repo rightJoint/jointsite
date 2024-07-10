c771a93c03308e84807e9fe8bb69f8a36df19d6e
Alexey <rightjoint@yandex.ru>
2024-07-10 19:37:58 +0300

    js-in-view

---
+++ application/views/siteman/music/musictrackstoalb/view_editview.php
@@ -0,0 +1,8 @@
+<?php
+//require_once $_SERVER["DOCUMENT_ROOT"] . JOINT_SITE_EXEC_DIR . "/application/views/templates/RecordView.php";
+//require_once $_SERVER["DOCUMENT_ROOT"] . JOINT_SITE_EXEC_DIR . "/application/views/templates/RecordEditView.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . JOINT_SITE_EXEC_DIR . "/application/views/test/records/musictrackstoalb/view_newview.php";
+class view_editview extends view_newview
+{
+
+}
