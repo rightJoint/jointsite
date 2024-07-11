e58fc0909564094b393343759577a0c69de1a936
Alexey <rightjoint@yandex.ru>
2024-07-11 10:24:43 +0300

    use-item-in-modal-menu

--- application/lang_files/views/lang_view_rus.php
+++ application/lang_files/views/lang_view_rus.php
@@ -38,6 +38,7 @@ class lang_view_rus
                     "get" => "<a href='https://github.com/rightJoint/jointsite/tree/main' title='скачать с гит хаба'>ссылка на github</a>",
                     "more" => "<a href='".JOINT_SITE_EXEC_DIR."/products/jointsite/main' title='узнать подробнее про приложение'>подробнее</a>",
                     "depend" => "нет зависимостей",
+                    "use_in_mm" => true,
                 ),
                 "record" => array(
                     "aliasMenu" => "Ветка Запись (Record)",
@@ -50,6 +51,7 @@ class lang_view_rus
                     "get" => "<a href='https://github.com/rightJoint/jointsite/tree/recnew' title='скачать с гит хаба'>ссылка на github</a>",
                     "more" => "<a href='".JOINT_SITE_EXEC_DIR."/products/jointsite/record' title='узнать подробнее про Запись'>подробнее</a>",
                     "depend" => "ветка main",
+                    "use_in_mm" => true,
                 ),
             ),
         ),
