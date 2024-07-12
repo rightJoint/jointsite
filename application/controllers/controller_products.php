e73a3c7f0ae96e16ca63f5a7f51d63c08dbe0a7f
Alexey <rightjoint@yandex.ru>
2024-07-12 10:44:58 +0300

    product-descr-recnew

--- application/controllers/controller_products.php
+++ application/controllers/controller_products.php
@@ -4,7 +4,7 @@ class Controller_Products extends Controller
     function action_jointsite()
     {
         global $request;
-        if($request["routes"][$request["exec_dir_cnt"]+2]){
+        if(isset($request["routes"][$request["exec_dir_cnt"]+2]) and $request["routes"][$request["exec_dir_cnt"]+2] != null){
             if(!in_array($request["routes"][$request["exec_dir_cnt"]+2], array_keys($this->view->branches))){
                 jointSite::throwErr("notFound", "product not found");
             }
