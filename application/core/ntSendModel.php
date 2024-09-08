f9ec0cb620f9b2889afe844cf50a711ec2b451ed
Alexey <rightjoint@yandex.ru>
2024-09-08 22:44:26 +0300

    ntf-test-prepare

--- application/core/ntSendModel.php
+++ application/core/ntSendModel.php
@@ -25,18 +25,14 @@ class ntSendModel extends Model_pdo
         $this->phpMailer->isHTML(PHP_MAILER_IS_HTML_1);
     }
 
-    public function AddNtf($templateName, $subscriber_type, $type_id, $template_params, $send_now, $method)
+    public function AddNtf(string $templateName, string $subscriber_type, string $type_id,
+                           $template_params, $send_now, $method):bool
     {
-      //  echo "AddNtf-1<br>";
-
         $findTemplate_qry = "select template_id from ntfTemplates_dt where template_id = '".$templateName."' or tName = '".$templateName."'";
         $findTemplate_res = $this->query($findTemplate_qry);
         if($findTemplate_res->rowCount() === 1){
-         //   echo "AddNtf==1<br>";
             $findTemplate_row = $findTemplate_res->fetch(PDO::FETCH_ASSOC);
-
             $newNtf_id = $this->createGUID();
-
             $addNtf_qry = "insert into ntfList_dt (".
                 "ntf_id, template_id, subscriber_type, type_id, add_date, template_params, send_params, send_date, created_by, send_res, send_log) values (".
                 "'".$newNtf_id."', '".$findTemplate_row["template_id"]."', '".$subscriber_type."', '".$type_id."', ".
@@ -48,14 +44,10 @@ class ntSendModel extends Model_pdo
                 return true;
             }
         }
-       // echo "AddNtf-end<br>";
-        //exit;
     }
 
     public function SendNtf($ntf_id, $method)
     {
-       // echo "SendNtf==1<br>";
-
         $sendList_qry = "select * from ntfList_dt where send_date is null";
         if($ntf_id){
             $sendList_qry.=" and ntf_id='".$ntf_id."'";
@@ -64,11 +56,7 @@ class ntSendModel extends Model_pdo
         $sendList_res = $this->query($sendList_qry);
 
         if($sendList_res->rowCount()){
-          //  echo "SendNtf? rowCount=".$sendList_res->rowCount()."<br>";
-
-
             while ($sendList_row = $sendList_res->fetch(PDO::FETCH_ASSOC)){
-
                 $send_res = array(
                     "result" => false,
                     "log" => null,
@@ -79,18 +67,13 @@ class ntSendModel extends Model_pdo
                 );
 
                 if($sendList_row["subscriber_type"] == "user"){
-                  //  echo "SendNtf for user<br>";
-
                     $sendUser_res = $this->sendNtfForUser($sendList_row["ntf_id"], $sendList_row["type_id"]);
                     foreach ($sendUser_res as $rK => $rV){
                         if($rV){
                             $send_res[$rK]+=1;
                         }
                     }
-                   // exit;
                 }elseif ($sendList_row["subscriber_type"] == "group"){
-                  //  echo "SendNtf for group<br>";
-                   // exit;
                     $groupUsers_qry = "select user_id from usersToGroups_dt where group_id='".$sendList_row["type_id"]."'";
                     $groupUsers_res = $this->query($groupUsers_qry);
                     if($groupUsers_res->rowCount()){
@@ -106,8 +89,6 @@ class ntSendModel extends Model_pdo
                         $send_res["log"] = "no subscribers in group<br>";
                     }
                 }elseif($sendList_row["subscriber_type"] == "email"){
-                  //  echo "SendNtf for email<br>";
-                   // exit;
                     $template_qry = "select ntfTemplates_dt.tHeader_en, ntfTemplates_dt.tBody_en, ntfList_dt.subscriber_type, ".
                         "ntfList_dt.type_id, ntfList_dt.send_params, ntfList_dt.template_params from ntfTemplates_dt ".
                         "inner join ntfList_dt on ntfTemplates_dt.template_id = ntfList_dt.template_id where ntfList_dt.ntf_id='".$ntf_id."'";
@@ -134,12 +115,9 @@ class ntSendModel extends Model_pdo
                             "foundMail" => 0,
                             "sendToEmail" => 1,
                         );
-
                     }
                 }
                 else{
-                  //  echo "SendNtf for unknown<br>";
-                  // exit;
                     $send_res["log"] = "unknown subscriber type<br>";
                 }
 
@@ -159,15 +137,10 @@ class ntSendModel extends Model_pdo
                 $this->query($updateNtfList_qry);
             }
         }
-
-        //echo "SendNtf: end<br>";
-        //exit;
     }
 
     function sendNtfForUser($ntf_id, $user_id)
     {
-        //echo "sendNtfForUser: begin<br>";
-
         $return = array(
             "put_ntf" => 0,
             "send_flag" => 0,
@@ -181,33 +154,22 @@ class ntSendModel extends Model_pdo
         $template_res = $this->query($template_qry);
 
         if($template_res->rowCount() === 1){
-            //echo "sendNtfForUser: rowCount==1<br>";
-            //exit;
             $template_row = $template_res->fetch(PDO::FETCH_ASSOC);
             if($template_row["subscriber_type"] == "group"){
-                //echo "sendNtfForUser: subscriber_type==group<br>";
-                //exit;
                 $findUserNtf_flag_qry = "select usersToGroups_dt.send_ntf, users_dt.eMail from usersToGroups_dt ".
                     "inner join users_dt on users_dt.user_id = usersToGroups_dt.user_id ".
                     "where usersToGroups_dt.user_id = '".$user_id."' and group_id = '".$template_row["type_id"]."'";
             }elseif ($template_row["subscriber_type"] == "user"){
-                //echo "sendNtfForUser: subscriber_type==user<br>";
-                //exit;
                 $findUserNtf_flag_qry = "select send_ntf, eMail from users_dt where user_id = '".$user_id."'";
             }
 
             $findUserNtf_flag_res = $this->query($findUserNtf_flag_qry);
 
             if($findUserNtf_flag_res->rowCount() === 1){
-
-                //echo "sendNtfForUser: findUserNtf_flag_res=rowCount==1<br>";
-                //exit;
-
                 $send_flag = "false";
                 $findUserNtf_flag_row = $findUserNtf_flag_res->fetch(PDO::FETCH_ASSOC);
 
                 if($findUserNtf_flag_row["send_ntf"]){
-                    //echo "sendNtfForUser: send_ntf-Ok<br>";
                     $return["send_flag"] = 1;
                     if($findUserNtf_flag_row["eMail"]){
                         $return["foundMail"] = 1;
@@ -221,25 +183,8 @@ class ntSendModel extends Model_pdo
                             }
                         }
                         $this->sendOnEmail($findUserNtf_flag_row["eMail"], $template_row["tHeader_en"], $replaced_text);
-
-                        //$this->phpMailer->Subject =
-
-
-                        //$this->phpMailer->Subject = "Заявка на " .$_SERVER["HTTP_HOST"]." принята "; // Заголовок письма
-                        /*$this->phpMailer->Body = "Спасибо что выбрали нашу компанию. Мы свяжемся с вами по eMail или телефону. Вы также всегда
-        можете отследить статус вашей заявки по ссылке https://".$_SERVER["HTTP_HOST"]."/applications/details/".
-                            $appl_rd["result"]["appl_id"]; // Текст письма*/
-
-
-
-
-
                     }
                 }
-
-                //echo "sendNtfForUser: insert ntfRead_dt<br>";
-                //exit;
-
                 $putNtf_qry = "insert into ntfRead_dt (ntf_id, user_id, read_date, put_date, send_flag) ".
                     "values (".
                     "'".$ntf_id."', ".
@@ -248,27 +193,20 @@ class ntSendModel extends Model_pdo
                     "'".date("Y-m-d H:i:s")."', ".
                     $send_flag.
                     ")";
-
-
-
                 $this->query($putNtf_qry);
 
                 $return["put_ntf"] = 1;
 
             }
         }
-        //echo "sendNtfForUser: end<br>";
-        //exit;
         return $return;
     }
 
     function sendOnEmail($email, $subject, $body)
     {
-        //cho "<br>send-on-e-mail<br>";
-        //exit;
         $this->phpMailer->addAddress($email); // Email получателя
         $this->phpMailer->Subject = $subject;
         $this->phpMailer->Body = $body;
-       /// $this->phpMailer->send();
+        $this->phpMailer->send();
     }
 }
