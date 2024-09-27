771486af8ddb1141aab007c3b5db900291041c5a
Alexey <rightjoint@yandex.ru>
2024-09-27 14:50:44 +0300

    prepare-test-url

--- src/application/controllers/controller_test.php
+++ src/application/controllers/controller_test.php
@@ -10,60 +10,12 @@ class controller_test extends RecordsController
 
     function action_index()
     {
-        global $app_log;
-        //echo "action-index";
-
-        //echo "<pre>";
-        //print_r($app_log);
-        //exit;
-
-        if($this->records_process())
-        {
-            echo "process-ok";
-        }else{
-            echo "process-fail";
-        }
+        $this->view->generate();
     }
 
-    function action_mirationstest()
+    function action_records()
     {
-        //$migrations_model = new model_migrations();
-        //$migrations_model->connect_db();
-        //if
-
-        exit;
-        $new_mirgation_test = new model_migrations();
-        $new_mirgation_test->getRecordStructure();
-
-        $test_migration_name = $new_mirgation_test->createGUID();
-        //$new_mirgation_test->recordStructureFields->record["migration_log_id"]["curVal"]=$new_mirgation_test->createGUID();
-        $new_mirgation_test->recordStructureFields->record["migration_name"]["curVal"] = $test_migration_name;
-        $new_mirgation_test->recordStructureFields->record["try_date"]["curVal"] = date("Y-m-d H:i:s");
-
-        //$cmd_1_text = "insert into migrations_log ".
-        //    "values migration_log_id = 'wwww', ".
-        //    "migration_name = 'test_migration_1', ".
-        //    "try_date = '".date("Y-m-d H:i:s")."', ".
-        //    "add_dat = '".date("Y-m-d H:i:s")."'";
-
-
-        $cmd_1_text = "insert into migrations_log (migration_log_id, migration_name, try_date, add_dat) ".
-            "values ('wwww', 'test_migration_1', '".date("Y-m-d H:i:s")."', ".date("Y-m-d H:i:s").")";
-
-        if($new_mirgation_test->insertMigration(array("cmd_1" => $cmd_1_text))){
-            //echo "insertMigration_ok";
-        }else{
-            //echo "insertMigration_fail: ".$new_mirgation_test->log_message;
-        }
-
-        $new_mirgation_test->copyRecord();
-        $cmd_1_text = "update migrations_log set migration_log_id='1111'";
-        $new_mirgation_test->recordStructureFields->record["try_date"]["curVal"] = date("Y-m-d H:i:s");
-        if($new_mirgation_test->updateMigration(array("cmd_1" => $cmd_1_text))){
-            echo "updateMigration_ok: ".$new_mirgation_test->log_message;
-        }else{
-            echo "updateMigration_fail: ".$new_mirgation_test->log_message;
-        }
-
+        echo "test-records";
     }
+
 }
--- src/application/views/view_test.php
+++ src/application/views/view_test.php
@@ -4,9 +4,9 @@ class view_test extends SiteView
     function __construct()
     {
         parent::__construct();
-        $this->styles[] = JOINT_SITE_EXEC_DIR."/lib/js/Elegant-Loading-Indicator-jQuery-Preloader/src/css/preloader.css";
+        //$this->styles[] = JOINT_SITE_EXEC_DIR."/lib/js/Elegant-Loading-Indicator-jQuery-Preloader/src/css/preloader.css";
 
-        $this->scripts[] = JOINT_SITE_EXEC_DIR."/lib/js/Elegant-Loading-Indicator-jQuery-Preloader/src/js/jquery.preloader.min.js";
+        //$this->scripts[] = JOINT_SITE_EXEC_DIR."/lib/js/Elegant-Loading-Indicator-jQuery-Preloader/src/js/jquery.preloader.min.js";
     }
 
     function print_page_content()
@@ -14,9 +14,13 @@ class view_test extends SiteView
         parent::print_page_content();
         echo "<div class='contentBlock-frame'><div class='contentBlock-center'>".
             "<div class='contentBlock-wrap'>".
+            "<div class='test-menu' style='text-align: left'>".
+            "<h2>Tests list</h2>".
             "<ul>".
             "<li><a href='".JOINT_SITE_EXEC_DIR.JOINT_SITE_APP_REF."/test/records'>Test Records</a></li>".
+            "<li><a href='".JOINT_SITE_EXEC_DIR.JOINT_SITE_APP_REF."/test/migrationstest'>Test Migrations</a></li>".
             "</ul>".
+            "</div>".
         "</div></div></div>";
     }
 }
