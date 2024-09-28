bb76cca1f440bec25940ea03a6f31a81b52efbf6
Alexey <rightjoint@yandex.ru>
2024-09-29 00:48:06 +0300

    del-not-uses

--- __config/app_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("USE_DEFAULT_CONTROLLER", false);
-define("USE_DEFAULT_MODEL", true);
-define("USE_DEFAULT_VIEW", true);
-define("USE_DEFAULT_ACTION", false);
--- __config/dir_const.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("JOINT_SITE_APP_CONFIG", JOINT_SITE_CONF_DIR."/app_config.php");
-define("SQL_CONN_DEFAULT", JOINT_SITE_CONF_DIR."/db_conn.php");
