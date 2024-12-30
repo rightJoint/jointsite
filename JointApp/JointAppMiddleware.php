0f682df7da48bfeddc4da40f82649783e29e79b6
Alexey <rightjoint@yandex.ru>
2024-12-30 10:04:40 +0300

    add-middleware

---
+++ JointApp/JointAppMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JointApp;
+
+use JointFramework\Http\ServerRequest;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class JointAppMiddleware implements MiddlewareInterface
+{
+
+    public ServerRequestInterface $request;
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        parse_str($request->getServerParams()["QUERY_STRING"], $REQ_ARR);
+
+        $this->request = $request
+            ->withCookieParams($_COOKIE)
+            ->withQueryParams($REQ_ARR)
+            ->withParsedBody($_POST);
+
+        return $handler->handle($this->request);
+    }
+}
+
