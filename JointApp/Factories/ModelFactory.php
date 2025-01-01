2504f1267582a40f8483469480d538ca9640016f
Alexey <rightjoint@yandex.ru>
2025-01-01 13:19:21 +0300

    add-ModelFactory

---
+++ JointApp/Factories/ModelFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace JointApp\Factories;
+
+
+use JointApp\JointAppRequest;
+
+class ModelFactory
+{
+    public static function createModelFromRequest(JointAppRequest $request, string $modelName, $modelParams = [])
+    {
+        if($modelName == 'JointApp\Models\Model'){
+            return new $modelName();
+        }else{
+            $model = new $modelName($request->docRoot, $request->configDir, $modelParams, $request->langLw);
+            return $model;
+        }
+    }
+
+    public static function createFromExistModel($newModelName, $existModelName, $newModelParams = [])
+    {
+        if($newModelName == 'JointApp\Models\Model'){
+            return new $newModelName();
+        }else{
+            $newModel = new $newModelName($existModelName->docRoot, $existModelName->configDir, $newModelParams, $existModelName->langLw);
+            return $newModel;
+        }
+    }
+}
