6bafa3d376ff8291e7bac8b6e702980dbf5d7f59
Alexey <rightjoint@yandex.ru>
2025-01-01 11:55:13 +0300

    add-lang-controllers-components

---
+++ JointApp/LangFiles/Controllers/Components/LangFiles_Ru_Controller_Components_Groups.php
@@ -0,0 +1,22 @@
+<?php
+
+class LangFiles_Ru_Controller_Components_Groups extends LangFiles_Ru_Controller_Records
+{
+    public $fieldAliases = [];
+
+    public function __construct()
+    {
+        $this->moduleAlias = 'Группы пользователей';
+        $this->fieldAliases = array(
+            'group_id' => 'id',
+            'groupAlias_en' => 'Алиас_en',
+            'groupAlias_ru' => 'Алиас_ру',
+            'activeFlag' => 'Исп',
+            'created_by' => 'создал_id',
+            'createdUser' => 'создал',
+        );
+
+    }
+
+
+}
---
+++ JointApp/LangFiles/Controllers/Components/LangFiles_Ru_Controller_Components_UsersToGroups.php
@@ -0,0 +1,38 @@
+<?php
+
+class LangFiles_Ru_Controller_Components_UsersToGroups extends LangFiles_Ru_Controller_Records
+{
+    //public $fieldAliases = [];
+
+
+    public function __construct()
+    {
+        $this->moduleAlias = 'Группы пользователей';
+
+        $this->fieldAliases = array(
+            'group_id' => 'ид гр.',
+            'user_id' => 'ид польз',
+            'read_rule' => 'чтение',
+            'create_rule' => 'создание',
+            'edit_rule' => 'редакт.',
+            'delete_rule' => 'удаление',
+            'created_by' => 'создал',
+            'created_user' => 'создал',
+            'send_ntf' => 'уведомл.',
+            'uses_alias' => 'пользователь',
+            'groupAlias' => 'группа',
+            'created_alias' => 'создал',
+        );
+
+        $this->rules = array(
+            'any' => 'любые',
+            'own' => 'свои',
+            'enable' => 'может',
+            'disable' => 'блокировано',
+            'forbidden' => 'запрещено',
+        );
+
+    }
+
+
+}
