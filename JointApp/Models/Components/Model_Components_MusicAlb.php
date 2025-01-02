dc82470fbe438248830a0952459f2a04706d4567
Alexey <rightjoint@yandex.ru>
2025-01-02 11:44:26 +0300

    custom-fields

--- JointApp/Models/Components/Model_Components_MusicAlb.php
+++ JointApp/Models/Components/Model_Components_MusicAlb.php
@@ -5,7 +5,7 @@ namespace JointApp\Models\Components;
 
 
 use JointApp\Models\ModuleModel;
-use JointApp\Models\Records\RecordsModel;
+use JointApp\JointAppQueryBuilder;
 
 class Model_Components_MusicAlb extends ModuleModel
 {
@@ -61,6 +61,33 @@ class Model_Components_MusicAlb extends ModuleModel
                 'format' => 'varchar',
                 'custom' => false,
             ),
+            'accAlias' => array(
+                'format' => 'varchar',
+                'curVal' => '',
+                'custom' => true,
+            ),
         );
+    }
+
+    public function listRecords(JointAppQueryBuilder $qBuilder): array
+    {
+        $qBuilder->select = $this->tableName.'.album_id, '.$this->tableName.'.albumName, '.
+            $this->tableName.'.albumAlias, '.$this->tableName.'.dateOfCr, '.$this->tableName.'.albumImg, '.
+            $this->tableName.'.activeFlag, '.$this->tableName.'.refreshDate, '.$this->tableName.'.robIndex, '.
+            'users_dt.accAlias ';
+        $qBuilder
+            ->from($this->tableName)
+            ->join('left join users_dt on '.$this->tableName.'.created_by = users_dt.user_id');
+        return $this->fetchToArray($qBuilder->buildQuery());
+    }
+
+    public function copyCustomFields(): bool
+    {
+        $findCreatedAlias_qry = 'select accAlias from users_dt where created_by="'.$this->record['created_by']['curVal'].'"';
+        $findCreatedAlias_arr = $this->fetchToArray($findCreatedAlias_qry);
+        if(count($findCreatedAlias_arr)){
+            $this->record['accAlias']['curVal'] = $findCreatedAlias_arr[0]['accAlias'];
+        }
+        return true;
     }
 }
