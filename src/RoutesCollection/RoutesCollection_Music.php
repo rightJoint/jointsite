482cc8a8055dd5b3cb005a2c07754b4848bf754b
Alexey <rightjoint@yandex.ru>
2025-01-04 17:58:38 +0300

    clear-code

--- src/RoutesCollection/RoutesCollection_Music.php
+++ src/RoutesCollection/RoutesCollection_Music.php
@@ -3,7 +3,7 @@
 
 namespace Src\RoutesCollection;
 
-//use JointApp\Controllers\Controller;
+
 use JointApp\Router\JointSiteRoute;
 
 trait RoutesCollection_Music
@@ -12,15 +12,9 @@ trait RoutesCollection_Music
     {
         $route = (new JointSiteRoute())
             ->withController('Src\Controllers\Controller_Music');
-        /*
-        $route = (new JointSiteRoute())
-            ->withController('Src\Controllers\Controller_User')
-            ->withModel('JointApp\Models\Model');
-        */
         if(!isset($routes_ns[2])){
             $route
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
-                ->withModel('Src\Models\Music\Model_Music_Main')
+                ->withModel('Src\Models\Music\Model_Music_Albums')
                 ->withAction('actionIndex')
                 ->withView('Src\Views\Music\View_Music_Main');
         }
@@ -28,8 +22,7 @@ trait RoutesCollection_Music
         elseif(strtolower($routes_ns[2]) == 'albums'){
             $route
                 ->withController('Src\Controllers\Controller_Music_Alb')
-                ->withModel('Src\Models\Music\Model_Music_Main')
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
+                ->withModel('Src\Models\Music\Model_Music_Albums')
                 ->withAction('getListView')
                 ->withView('Src\Views\Music\View_Music_Albums');
         }
@@ -38,23 +31,15 @@ trait RoutesCollection_Music
             $route
                 ->withController('Src\Controllers\Controller_Music_Tracks')
                 ->withModel('Src\Models\Music\Model_Music_Tracks')
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
                 ->withAction('getListView')
                 ->withView('Src\Views\Music\View_Music_Tracks');
         }
-
-        /*elseif(strtolower($routes_ns[2]) == 'signup'){
-            $route
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
-                ->withAction('actionGetSignUp')
-                ->withView('Src\Views\User\View_User_SignUp');
-        }*/
         return $route;
-
     }
 
     static function postRoute_Music($routes_ns):JointSiteRoute
     {
+        //POST: /music/tracks
         if(strtolower($routes_ns[2]) == 'tracks') {
             if (!isset($routes_ns[3]) or strtolower($routes_ns[3]) == 'listview') {
                 $route = (new JointSiteRoute())
@@ -65,23 +50,19 @@ trait RoutesCollection_Music
                     ->responseFormat('json');
             }
         }
-        /*
-        $route = (new JointSiteRoute())
-            ->withController('Src\Controllers\Controller_User')
-            ->withModel('Src\Models\Model_User');
-        if(isset($routes_ns[2]) and (strtolower($routes_ns[2]) == 'signin')){
+        //POST: /music/albums
+        if(strtolower($routes_ns[2]) == 'albums') {
+            if (!isset($routes_ns[3]) or strtolower($routes_ns[3]) == 'listview') {
 
-            $route
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
-                ->withAction('actionPostSignIn')
-                ->withView('Src\Views\User\View_User_SignIn');
+                $route = (new JointSiteRoute())
+                    ->withController('Src\Controllers\Controller_Music_Alb')
+                    ->withAction('applyFilterView')
+                    ->withModel('Src\Models\Music\Model_Music_Albums')
+                    ->withView('Src\Views\Music\View_Music_Albums')
+                    ->responseFormat('json');
 
-        }elseif(isset($routes_ns[2]) and strtolower($routes_ns[2]) == 'signup'){
-            $route
-                //->withController('Src\Controllers\Controller_User', ['actionName' => 'actionIndex'])
-                ->withAction('actionPostSignUp')
-                ->withView('Src\Views\User\View_User_SignUp');
-        }*/
+            }
+        }
         return $route;
 
     }
