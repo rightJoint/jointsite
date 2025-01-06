e0c39f419815774aeb9f7974cd3ada7a57f35e19
Alexey <rightjoint@yandex.ru>
2025-01-06 12:24:57 +0300

    test-use-model

--- src/RoutesCollection/RoutesCollection_Test.php
+++ src/RoutesCollection/RoutesCollection_Test.php
@@ -15,7 +15,7 @@ trait RoutesCollection_Test
             $route
                 ->withController('Src\Controllers\Controller_Test')
                 ->withAction('actionIndex')
-                ->withModel('JointApp\Models\Model\Model')
+                ->withModel('JointApp\Models\Model')
                 ->withView('Src\Views\Test\View_Test');
         }
         //GET: test/migrations
@@ -85,7 +85,7 @@ trait RoutesCollection_Test
             $route
                 ->withController('Src\Controllers\Controller_Test')
                 ->withAction('actionFiles')
-                ->withModel('JointApp\Models\Model\Model')
+                ->withModel('JointApp\Models\Model')
                 ->withView('Src\Views\Test\View_Test_Files');
             return $route;
             //return self::getRoute_TestEmail($routes_ns);
@@ -101,7 +101,7 @@ trait RoutesCollection_Test
             $route
                 ->withController('Src\Controllers\Test\Controller_Test_MigrationsTest')
                 ->withAction('action_index')
-                ->withModel('JointApp\Models\Model\Model')
+                ->withModel('JointApp\Models\Model')
                 ->withView('Src\Views\Test\Migrations\View_Test_Migrations');
         }
         //GET: test/migrations/checkconnectserverstatus
