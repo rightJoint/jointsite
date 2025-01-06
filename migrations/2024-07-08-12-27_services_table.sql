e9a1e1bcb1a57525a712862ae671727065d0f015
Alexey <rightjoint@yandex.ru>
2025-01-06 19:24:07 +0300

    services-tables

---
+++ migrations/2024-07-08-12-27_services_table.sql
@@ -0,0 +1,21 @@
+create table srvCards_dt (
+    card_id int(5) AUTO_INCREMENT,
+    cardName_ru varchar(128) collate utf8_unicode_ci not null,
+    cardName_en varchar(128) collate utf8_unicode_ci,
+    cardAlias varchar(128) collate utf8_unicode_ci not null,
+    shortDescr_ru TEXT collate utf8_unicode_ci,
+    shortDescr_en TEXT collate utf8_unicode_ci,
+    longDescr_ru TEXT collate utf8_unicode_ci,
+    longDescr_en TEXT collate utf8_unicode_ci,
+    cardImg varchar(128) collate utf8_unicode_ci,
+    cardActive BOOLEAN, 
+    cardPrice_ru int(5) not null,
+    cardPrice_en int(5) not null,
+    cardCurr_ru varchar(16),
+    cardCurr_en varchar(16),
+    sortDate date not null,
+    unit_ru varchar(8) collate utf8_unicode_ci,
+    unit_en varchar(8) collate utf8_unicode_ci,
+    created_by varchar(36) not null collate utf8_unicode_ci,
+    primary key (card_id)
+    ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
