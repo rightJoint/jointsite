5cd9c9873fc7b04c683ad562a883afd1e757d24c
Alexey <rightjoint@yandex.ru>
2025-01-06 10:41:40 +0300

    playAlb-route

--- src/RoutesCollection/RoutesCollection_Music.php
+++ src/RoutesCollection/RoutesCollection_Music.php
@@ -26,13 +26,23 @@ trait RoutesCollection_Music
                 ->withAction('getListView')
                 ->withView('Src\Views\Music\View_Music_Albums');
         }
-        //GET: /music/albums
+        //GET: /music/tracks
         elseif(strtolower($routes_ns[2]) == 'tracks'){
             $route
                 ->withController('Src\Controllers\Controller_Music_Tracks')
                 ->withModel('Src\Models\Music\Model_Music_Tracks')
                 ->withAction('getListView')
                 ->withView('Src\Views\Music\View_Music_Tracks');
+        }
+        //GET: /music/playalbum
+        elseif(strtolower($routes_ns[2]) == 'playalbum'){
+
+            $route
+                ->withController('Src\Controllers\Controller_Music', ['playAlbName' => $routes_ns[3]])
+                ->withModel('Src\Models\Music\Model_Music_Albums')
+                ->withAction('actionPlayAlbum')
+                ->withView('Src\Views\Music\View_Music_PlayAlb');
+
         }
         return $route;
     }
@@ -43,7 +53,7 @@ trait RoutesCollection_Music
         if(strtolower($routes_ns[2]) == 'tracks') {
             if (!isset($routes_ns[3]) or strtolower($routes_ns[3]) == 'listview') {
                 $route = (new JointSiteRoute())
-                    ->withController('Src\Controllers\Controller_Music_Tracks')
+                    ->withController('Src\Controllers\Controller_Music_Tracks', )
                     ->withAction('applyFilterView')
                     ->withModel('Src\Models\Music\Model_Music_Tracks')
                     ->withView('Src\Views\Music\View_Music_Tracks')
