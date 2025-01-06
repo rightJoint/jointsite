c983bcd3b1603770e52e05a67d1b460f0cd9a6b2
Alexey <rightjoint@yandex.ru>
2025-01-06 10:47:19 +0300

    index

--- src/index.php
+++ src/index.php
@@ -1,13 +1,32 @@
 <?php
 
+//require_once "JobInterviewTest/JobInterviewTest.php";
+//$job_nterview_test = new JobInterviewTest();
+//exit;
+
 require __DIR__ . '/vendor/autoload.php';
 
-use JointSite\Core\Logger\JointSiteLogger;
-$JointSite_Logger = new JointSiteLogger();
+use JointFramework\Http\Uri;
+use JointFramework\Http\ServerRequestFactory;
+use JointApp\JointSite;
+use JointApp\JointAppMiddleware;
 
-use JointSite\Core\JointSite;
 
-$jointSite = new JointSite();
-$jointSite->document_root = $_SERVER["DOCUMENT_ROOT"];
-$jointSite->request_uri = $_SERVER["REQUEST_URI"];
-$jointSite->jsRun();
+use JointApp\JointAppMailer;
+
+$uri = new Uri($_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI']);
+
+//create server request from server params
+$factory = new ServerRequestFactory();
+$request = $factory->createServerRequest($_SERVER['REQUEST_METHOD'], $uri, $_SERVER);
+
+//create and process middleware
+$middleware = new JointAppMiddleware();
+
+//create server request handler
+$handler = new JointSite();
+
+$response = $middleware->process($request, $handler);
+
+$jointSiteRequest = $handler::requestAdapter($middleware->request);
+JointSite::handleResponse($jointSiteRequest, $response);
