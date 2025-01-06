4f68984014eb6f8bbed480609265224d85d905db
Alexey <rightjoint@yandex.ru>
2025-01-06 11:23:14 +0300

    add-JointAppResponse

---
+++ JointApp/JointAppResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace JointApp;
+
+
+use JointApp\Interfaces\ResponseAdapterInterface;
+use JointFramework\Http\Response;
+use Psr\Log\LogLevel;
+
+class JointAppResponse extends Response implements ResponseAdapterInterface
+{
+
+    //log err included 200 status codes: DEBUG, NOTICE, INFO
+    public $customLog = [];
+
+    //store run time of JointApp, Action, View
+    public $stopwatch = [];
+
+    //defined in JointSiteRoute RoutesCollection traits
+    //text format, or json
+    public $responseFormat = 'text';
+
+    //used view text response
+    public string $responseText = '';
+    //used view json response
+    public $responseJson = [];
+
+    //list of redirects if need redirect
+    //for example when add (newView)
+    //or delete (deleteView) record, then redirect to listView
+    public $redirect = [];
+
+    //list of redirects if need redirect
+    //for example when add (newView)
+    //or delete (deleteView) record, then redirect to listView
+    public function redirect(string $location):void
+    {
+        $this->redirect[] = $location;
+    }
+}
