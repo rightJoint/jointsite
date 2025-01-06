01268be49ed7d14d8e96ea0634edf1657101a334
Alexey <rightjoint@yandex.ru>
2025-01-06 12:37:23 +0300

    add-lang-model-test-tables

---
+++ src/LangFiles/Models/Test/LangFiles_Ru_Models_Test_Tables.php
@@ -0,0 +1,16 @@
+<?php
+class LangFiles_Ru_Models_Test_Tables extends LangFiles_Ru_Model
+{
+    public $admin_mlm = array(
+        "auth_err_fnv"=> "файл с пользователями неправильного формата или пустой",
+        "auth_err_fnf"=> "файл с пользователями не найден",
+        "upload_table" => array(
+            "noting" => "пустая - нет записей",
+            "write" => "не сохраняется в файл",
+            "success" => "успешно",
+            "upload_all" => "Действие: выгрузить все",
+            "pt" => "Приставка",
+            "dt" => "Дт.штамп",
+        ),
+    );
+}
