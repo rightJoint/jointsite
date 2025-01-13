0dc68eec9262007ce36a6ec32346be2038629727
Alexey <rightjoint@yandex.ru>
2025-01-13 12:42:26 +0300

    print-main

--- JointApp/Views/User/UserSubMenu.php
+++ JointApp/Views/User/UserSubMenu.php
@@ -6,13 +6,19 @@ namespace JointApp\Views\User;
 
 class UserSubMenu
 {
-    public static function printUserSubMenu($text):string
+    public static function printUserSubMenu(array $subMenuMain, array $routes_ns, $subMenuText = ''):string
     {
+        $mainMenu_class = '';
+        if(!isset($routes_ns[2]) or empty($routes_ns[2])){
+            $mainMenu_class = 'class="active"';
+        }
+
         return '<div class="contentBlock-frame"><div class="contentBlock-center">'.
             '<div class="contentBlock-wrap">'.
             '<div class="user-sub-menu">'.
             '<ul>'.
-            $text.
+            '<li><a href="/user" title="'.$subMenuMain['refTitle'].'" '.$mainMenu_class.'>'.$subMenuMain['refText'].'</a></li>'.
+            $subMenuText.
             '</ul>'.
             '</div>'.
             '</div></div></div>'.
