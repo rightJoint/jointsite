265cdd1469ee9bd3915d7e8a9e5d0376e3fc3057
Alexey <rightjoint@yandex.ru>
2025-01-13 12:38:37 +0300

    use-record-controller

--- JointApp/Controllers/User/Controller_User_Account.php
+++ JointApp/Controllers/User/Controller_User_Account.php
@@ -4,20 +4,14 @@
 namespace JointApp\Controllers\User;
 
 
-use JointApp\Controllers\Controller;
+use JointApp\Controllers\Records\RecordsController;
 use JointApp\Views\User\UserSubMenu;
 
-class Controller_User_Account extends Controller
+class Controller_User_Account extends RecordsController
 {
-    public array $editFields = [];
-
     public function loadLangController(): string
     {
-
         parent::loadLangController();
-        $name = 'LangFiles_'.$this->langNs.'_Controller_Records';
-        require_once $this->docRoot.'/JointApp/LangFiles/Controllers/'.$name.'.php';
-
         $name = 'LangFiles_'.$this->langNs.'_Controller_User_Account';
         require_once $this->docRoot.'/JointApp/LangFiles/Controllers/User/'.$name.'.php';
         return $name;
@@ -27,7 +21,7 @@ class Controller_User_Account extends Controller
     {
 
         $subMenu = $this->view::printMenuItems($this->langMap->userSubMenuItems, '/user', $this->langSl, $this->routes_ns);
-        $contentMenu = UserSubMenu::printUserSubMenu($subMenu['text']);
+        $contentMenu = UserSubMenu::printUserSubMenu($this->langMap->userSubMenuItems['main'], $this->routes_ns, $subMenu['text']);
 
         $this->view->putPageContentBefore($contentMenu);
     }
