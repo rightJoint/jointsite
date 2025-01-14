e70330c6d7b85ea3152e2ddaf4e14b58c56dcac8
Alexey <rightjoint@yandex.ru>
2025-01-14 15:26:41 +0300

    template-params

--- JointApp/Views/HtmlInputs/HtmlEmailType.php
+++ JointApp/Views/HtmlInputs/HtmlEmailType.php
@@ -8,18 +8,15 @@ class HtmlEmailType extends HtmlInputView
 {
     public function htmlInput()
     {
-        /*
-        $template_params = json_decode($template_row["template_params"], true);
-        $replaced_text = $template_row["tBody_en"];
+        $template_params = json_decode($this->fieldOptions["template_params"], true);
+
+        $replaced_text = $this->value_print;
 
         if(isset($template_params) and is_array($template_params)){
             foreach ($template_params as $tp_key => $tp_val){
                 $replaced_text = str_replace("$^".$tp_key, $tp_val, $replaced_text);
             }
         }
-        */
-
-
-        $this->return_input = '<div class="email-template">'.$this->value_print.'</div>';
+        $this->return_input = '<div class="email-template">'.$replaced_text.'</div>';
     }
 }
