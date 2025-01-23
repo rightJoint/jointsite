8eeb17d836347746faa5042817d45b6946abdaa6
Alexey <rightjoint@yandex.ru>
2025-01-23 15:34:36 +0300

    api-records

--- src/Views/JointSite/About/View_JointSite_About_Api.php
+++ src/Views/JointSite/About/View_JointSite_About_Api.php
@@ -15,12 +15,119 @@ class View_JointSite_About_Api extends View_JointSite_About
         return parent::loadLangView($docRoot, $viewLang, $loads);
     }
 
+    public static function addStyleLinks(callable $addStyleLinks):void
+    {
+        parent::addStyleLinks($addStyleLinks);
+
+        $addStyleLinks([
+            '/css/jointSite/diffTable.css',
+        ]);
+    }
+
     //content after menu
     protected static function pageContentJointSite(\stdClass $contentBlock, string $langSl = ''):string
     {
         return '<section>'.
+            '<h3>Типы апи в приложении</h3>'.
             '<p>'.
-            'материал подготавливается к публикации'.
+            'Приложение поддерживает два типа rest json апи:'.
+            '<ul>'.
+            '<li>Records api на основе RecordsModel и RecordsController</li>'.
+            '<li>Module api на основе ModuleModel и ModuleController</li>'.
+            '</ul>'.
+            '</p>'.
+            '<p>'.
+            '<ul>'.
+            'Методы АПИ: '.
+            '<li>get - получить запись или список записей</li>'.
+            '<li>put - добавить запись</li>'.
+            '<li>delete - удалить запись</li>'.
+            '<li>post - обновить запись</li>'.
+            '</ul>'.
+            '</p>'.
+            '</section>'.
+            '<section>'.
+            '<h3>Доступы АПИ</h3>'.
+            '<p>'.
+            'Для доступа к Records api вы должны передовать параметр <b>record_api_access_token</b> '.
+            'тем методом, которым обращаетесь. '.
+            '</p>'.
+            '<p>'.
+            'Для доступа к Module api, ...'.
+            '</p>'.
+            '</section>'.
+            '</section>'.
+            '<section>'.
+            '<h3>Uri для запросов</h3>'.
+            '<p>'.
+            'Маршруты к Records api, как и другие маршруты, задаются в RoutesCollection, RoutesCollection_Api'.
+            '<div class="diffTable">'.
+            '<table>'.
+            '<tr>'.
+            '<td>Метод</td>'.
+            '<td>Маршрут</td>'.
+            '<td>Метод контроллера</td>'.
+            '<td>Примечание</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/tableName</td>'.
+            '<td>getListRecords</td>'.
+            '<td>list records</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/tableName/list</td>'.
+            '<td>getListRecords</td>'.
+            '<td>list records</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/tableName/detail</td>'.
+            '<td>getDetailRecord</td>'.
+            '<td>detail record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>put</td>'.
+            '<td>/api/records/tableName</td>'.
+            '<td>putRecord</td>'.
+            '<td>create record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>put</td>'.
+            '<td>/api/records/tableName/new</td>'.
+            '<td>putRecord</td>'.
+            '<td>create record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>delete</td>'.
+            '<td>/api/records/tableName</td>'.
+            '<td>deleteRecord</td>'.
+            '<td>delete record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>delete</td>'.
+            '<td>/api/records/tableName/delete</td>'.
+            '<td>deleteRecord</td>'.
+            '<td>delete record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>post</td>'.
+            '<td>/api/records/tableName</td>'.
+            '<td>editRecord</td>'.
+            '<td>edit record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>post</td>'.
+            '<td>/api/records/tableName/edit</td>'.
+            '<td>editRecord</td>'.
+            '<td>edit record</td>'.
+            '</tr>'.
+            '</table>'.
+            '</div>'.
+            '</p>'.
+            '<p>'.
+            'Для доступа к Module api, ...'.
             '</p>'.
             '</section>';
     }
---
+++ src/__config/apiAccessToken.php
@@ -0,0 +1,4 @@
+<?php
+class apiAccessToken{
+    const TOKEN = '123';
+}
