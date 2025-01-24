673227ee4007e7a0dc3ffab20d0c672db4301157
Alexey <rightjoint@yandex.ru>
2025-01-24 10:38:50 +0300

    mudule-api-text

--- src/Views/JointSite/About/View_JointSite_About_Api.php
+++ src/Views/JointSite/About/View_JointSite_About_Api.php
@@ -45,22 +45,29 @@ class View_JointSite_About_Api extends View_JointSite_About
             '<li>post - обновить запись</li>'.
             '</ul>'.
             '</p>'.
+            '<p>'.
+            'Для получения списка записей можно передавать дополнительные параметры page - номер страницы, onPage - количество записей на странице'.
+            '</p>'.
             '</section>'.
             '<section>'.
             '<h3>Доступы АПИ</h3>'.
             '<p>'.
             'Для доступа к Records api вы должны передовать параметр <b>record_api_access_token</b> '.
-            'тем методом, которым обращаетесь. '.
+            'тем методом, которым обращаетесь. Контроллер сравнивает переданный параметр с константой в классе <b>/__config/apiAccessToken.php TOKEN</b> '.
+            'и предоставляет доступ при совпадении.'.
             '</p>'.
             '<p>'.
-            'Для доступа к Module api, ...'.
+            'Для доступа к Module api требуется авторизация, постому что доступы могут быть назначенны только на созданные '.
+            'пользователем объяекты и ModuleModel работает с полем created_by. '.
+            'Параметры <b>module_api_login</b> и <b>module_api_password</b> должны передаваться в запросе тем методом, которым обращаетесь.'.
             '</p>'.
             '</section>'.
             '</section>'.
             '<section>'.
             '<h3>Uri для запросов</h3>'.
             '<p>'.
-            'Маршруты к Records api, как и другие маршруты, задаются в RoutesCollection, RoutesCollection_Api'.
+            'Маршруты к Records api, как и другие маршруты, задаются в RoutesCollection, трэйт RoutesCollection_Api. '.
+            'Для получения ответа на английском, добавьте перед uri язык /en'.
             '<div class="diffTable">'.
             '<table>'.
             '<tr>'.
@@ -127,8 +134,79 @@ class View_JointSite_About_Api extends View_JointSite_About
             '</div>'.
             '</p>'.
             '<p>'.
-            'Для доступа к Module api, ...'.
+            'Вместо tableName вам надо подставить имя таблицы из базы данных, например musicTracksToAlb.'.
             '</p>'.
+            '<p>'.
+            'Маршруты к Module api так же задаются в RoutesCollection, трэйт RoutesCollection_Api'.
+            '<div class="diffTable">'.
+            '<table>'.
+            '<tr>'.
+            '<td>Метод</td>'.
+            '<td>Маршрут</td>'.
+            '<td>Метод контроллера</td>'.
+            '<td>Примечание</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/moduleName</td>'.
+            '<td>getListRecords</td>'.
+            '<td>list records</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/moduleName/list</td>'.
+            '<td>getListRecords</td>'.
+            '<td>list records</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>get</td>'.
+            '<td>/api/records/moduleName/detail</td>'.
+            '<td>getDetailRecord</td>'.
+            '<td>detail record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>put</td>'.
+            '<td>/api/records/moduleName</td>'.
+            '<td>putRecord</td>'.
+            '<td>create record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>put</td>'.
+            '<td>/api/records/moduleName/new</td>'.
+            '<td>putRecord</td>'.
+            '<td>create record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>delete</td>'.
+            '<td>/api/records/moduleName</td>'.
+            '<td>deleteRecord</td>'.
+            '<td>delete record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>delete</td>'.
+            '<td>/api/records/moduleName/delete</td>'.
+            '<td>deleteRecord</td>'.
+            '<td>delete record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>post</td>'.
+            '<td>/api/records/moduleName</td>'.
+            '<td>editRecord</td>'.
+            '<td>edit record</td>'.
+            '</tr>'.
+            '<tr>'.
+            '<td>post</td>'.
+            '<td>/api/records/moduleName/edit</td>'.
+            '<td>editRecord</td>'.
+            '<td>edit record</td>'.
+            '</tr>'.
+            '</table>'.
+            '</div>'.
+            '</p>'.
+            '<p>'.
+            'Вместо moduleName вам надо подставить имя компонента, например MusicTracksToAlb.'.
+            '</p>'.
+
             '</section>';
     }
 }
