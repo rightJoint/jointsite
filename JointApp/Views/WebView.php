a2bb5611f6c008f2d78483410b183904798284ed
Alexey <rightjoint@yandex.ru>
2025-01-27 17:22:46 +0300

    social-avatar

--- JointApp/Views/WebView.php
+++ JointApp/Views/WebView.php
@@ -13,7 +13,7 @@ use Psr\Log\LoggerAwareTrait;
 
 class WebView extends View implements WebViewInterface
 {
-    const USER_AVATARS_DIR = '/userdata/avatars/';
+    const USER_AVATARS_DIR = '/userdata/avatars';
     use LoggerAwareTrait;
 
     //**************CONSTRUCT PARAMS*********************************/
@@ -635,7 +635,11 @@ class WebView extends View implements WebViewInterface
             $user = '<div class="modal-line">'.
                 '<div class="modal-line-img"><img src="';
             if(!empty($currentUser->photoLink)){
-                $user.= self::USER_AVATARS_DIR.$currentUser->photoLink;
+                if($currentUser->network == 'site'){
+                    $user.= self::USER_AVATARS_DIR.'/'.$currentUser->photoLink;
+                }else{
+                    $user.= $currentUser->photoLink;
+                }
             }else{
                 $user.= '/img/popimg/avatar-default.png';
             }
