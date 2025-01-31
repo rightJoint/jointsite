3568ca9a2af05e6b5aa25c0ffff0da3ae9e4af4d
Alexey <rightjoint@yandex.ru>
2025-01-31 21:43:19 +0300

    blog-new-cats

---
+++ migrations/2025_01_31-blog-new-cats.sql
@@ -0,0 +1,5 @@
+insert into blogCats (
+cat_id, catAlias, catName_en, catName_ru, catMeta_en, catMeta_ru, catImg, activeFlag, indexFlag, created_by)
+ values 
+('6C20AC2A-7817-4440-A67D-5A3D40471275', 'it', 'it', 'it', 'it-блог. Статьи и посты на различные темы в it-сфере', 'it-блог. Статьи и посты на различные темы в it-сфере', '56B4B593-11DD-4287-A52E-F8F94DF01126.jpg', '1', '1', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'), 
+('3D56453C-C00E-4241-A2AE-9A6A1993E6A6', 'job', 'job', 'работа', 'job-descr', 'работа-описание', '8EE9AEEC-FD6F-4C12-AEAC-9E8540072F1A.jpg', '1', '1', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E');
