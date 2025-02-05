d8ae87fd0fbbaf9a38f5c47305e0d9729257b138
Alexey <rightjoint@yandex.ru>
2025-02-05 12:00:35 +0300

    lang-comments

--- src/LangFiles/Views/Blog/LangFiles_En_Views_BlogArts.php
+++ src/LangFiles/Views/Blog/LangFiles_En_Views_BlogArts.php
@@ -33,11 +33,73 @@ class LangFiles_En_Views_BlogArts extends LangFiles_En_Views_SiteView
 
         $langPageContent->langArtContent = static::getLangArtContent();
 
+        $formComments = new stdClass();
+        $formComments->socialButtons = self::blogSocicalButtons();
+        $langPageContent->formComments = $formComments;
+        $formComments->artCommentNew = 'Post New comment';
+        $formComments->postNew = 'Post New';
+        $formComments->postAnswer = 'Post answer';
+        $formComments->postErrText = 'Error';
+
+        $langPageContent->landFilter = self::getLangCommentsFilter();
+
+        $langPageContent->commentsH3 = 'Comments';
+
+        $langPageContent->langList = self::getLangCommentsList();
+
         return $langPageContent;
+    }
+
+    static public function blogSocicalButtons():\stdClass
+    {
+        $socialButtons = self::socialButtons();
+
+        $mail = new stdClass();
+        $mail->title = 'jointsite account';
+        $mail->alt = 'Using login and password';
+        $socialButtons->mail = $mail;
+
+        $socialButtons->defaultText = 'Sign In to write comments';
+
+        return $socialButtons;
     }
 
     static public function getLangArtContent():\stdClass
     {
         return new stdClass();
+    }
+
+    static public function getLangCommentsFilter():\stdClass
+    {
+        $landFilter = new \stdClass();
+        $landFilter->labelFound = 'Found';
+        $landFilter->labelSort = 'Sort';
+        $landFilter->labelOnPage = 'On page';
+        $landFilter->labelViewType = 'View type';
+
+        $landFilter->optByDate = 'by date';
+        $landFilter->optNewTop = 'new on top';
+        $landFilter->optLikePlus = 'likes plus';
+        $landFilter->optLikeMinus = 'likes minus';
+
+        $landFilter->opTypeList = 'List';
+        $landFilter->opTypeTree = 'Tree';
+
+        $langPg = new \stdClass();
+        $langPg->next = 'next';
+        $langPg->pre = 'pre';
+
+        $landFilter->langPg = $langPg;
+
+        return $landFilter;
+    }
+
+    static public function getLangCommentsList():stdClass
+    {
+        $langList = new \stdClass();
+        $langList->respLink = 'Respond';
+        $langList->respTest = 'answ';
+        $langList->firstComment = 'Add comment first!';
+        return $langList;
     }
 }
--- src/LangFiles/Views/Blog/LangFiles_Ru_Views_BlogArts.php
+++ src/LangFiles/Views/Blog/LangFiles_Ru_Views_BlogArts.php
@@ -33,11 +33,74 @@ class LangFiles_Ru_Views_BlogArts extends LangFiles_Ru_Views_SiteView
 
         $langPageContent->langArtContent = static::getLangArtContent();
 
+        $formComments = new stdClass();
+        $formComments->socialButtons = self::blogSocicalButtons();
+        $formComments->artCommentNew = 'Написать новый коммент';
+        $formComments->postNew = 'Написать';
+        $formComments->postAnswer = 'Ответить';
+        $formComments->postErrText = 'ОШИБКА';
+
+        $langPageContent->formComments = $formComments;
+
+        $langPageContent->landFilter = self::getLangCommentsFilter();
+
+        $langPageContent->commentsH3 = 'Комментарии';
+
+        $langPageContent->langList = self::getLangCommentsList();
+
         return $langPageContent;
+    }
+
+    static public function blogSocicalButtons():\stdClass
+    {
+        $socialButtons = self::socialButtons();
+
+        $mail = new stdClass();
+        $mail->title = 'Через аккаунт на сайте';
+        $mail->alt = 'Вход по логин и пароль';
+        $socialButtons->mail = $mail;
+
+        $socialButtons->defaultText = 'Зарегистрируйтесь что бы писать комменты';
+
+        return $socialButtons;
     }
 
     static public function getLangArtContent():\stdClass
     {
         return new stdClass();
+    }
+
+    static public function getLangCommentsFilter():\stdClass
+    {
+        $landFilter = new \stdClass();
+        $landFilter->labelFound = 'Найдено';
+        $landFilter->labelSort = 'Сортировка';
+        $landFilter->labelOnPage = 'Показывать по';
+        $landFilter->labelViewType = 'Вид списка';
+
+        $landFilter->optByDate = 'По дате';
+        $landFilter->optNewTop = 'Сначала новые';
+        $landFilter->optLikePlus = 'Больше лайков';
+        $landFilter->optLikeMinus = 'Меньше лайков';
+
+        $landFilter->opTypeList = 'Список';
+        $landFilter->opTypeTree = 'Дерево';
+
+        $langPg = new \stdClass();
+        $langPg->next = 'след.';
+        $langPg->pre = 'пред.';
+
+        $landFilter->langPg = $langPg;
+
+        return $landFilter;
+    }
+
+    static public function getLangCommentsList():stdClass
+    {
+        $langList = new \stdClass();
+        $langList->respLink = 'Ответить';
+        $langList->respTest = 'ответ.';
+        $langList->firstComment = 'Напишите коммент первым!';
+        return $langList;
     }
 }
