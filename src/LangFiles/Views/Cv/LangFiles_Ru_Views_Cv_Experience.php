a5c748df13f8186ccdeca70c7eb76e1c780eba2d
Alexey <rightjoint@yandex.ru>
2025-03-04 15:59:20 +0300

    add-cv-exp

---
+++ src/LangFiles/Views/Cv/LangFiles_Ru_Views_Cv_Experience.php
@@ -0,0 +1,20 @@
+<?php
+class LangFiles_Ru_Views_Cv_Experience extends LangFiles_Ru_Views_Cv_Main
+{
+    static public function getLangHead():stdClass
+    {
+        $langHead = parent::getLangHead();
+
+        $langHead->description = 'Мой опыт работы, профессиональные навыки, ответы на часто задаваемые вопросы.';
+        $langHead->title = 'Резюме - опыт работы';
+
+        return $langHead;
+    }
+    static public function getLangHeader():stdClass
+    {
+        $langHeader = parent::getLangHeader();
+        $langHeader->h1 = 'Резюме - опыт работы';
+        return $langHeader;
+    }
+
+}
