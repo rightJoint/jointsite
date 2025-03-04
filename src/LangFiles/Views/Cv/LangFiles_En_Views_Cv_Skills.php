4874ca0e0d6cf62b465e9b0c33dfae929540c3b0
Alexey <rightjoint@yandex.ru>
2025-03-04 15:57:55 +0300

    cv-skills

---
+++ src/LangFiles/Views/Cv/LangFiles_En_Views_Cv_Skills.php
@@ -0,0 +1,20 @@
+<?php
+class LangFiles_En_Views_Cv_Skills extends LangFiles_En_Views_Cv_Main
+{
+    static public function getLangHead():stdClass
+    {
+        $langHead = parent::getLangHead();
+
+        $langHead->description = 'Оченка собственных профессиональных навыков, которые чаще всего требуют в описаниях к вакансиям';
+        $langHead->title = 'Резюме - профессиональные навыки';
+
+        return $langHead;
+    }
+    static public function getLangHeader():stdClass
+    {
+        $langHeader = parent::getLangHeader();
+        $langHeader->h1 = 'Резюме - профессиональные навыки';
+        return $langHeader;
+    }
+
+}
